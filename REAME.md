9882594937d2bbaf882c1e8cd2f97dded2033fcc
BrunoTerre <brunoeterres@gmail.com>
2021-02-23 15:22:04 -0300

    first commit

---
+++ REAME.md
@@ -0,0 +1,1 @@
+A Dockerized Django version of Be The Hero Project made originally in JS, Node and React at Omnistack bootcamp by Rocketseat
---
+++ app/be_the_hero/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for be_the_hero project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'be_the_hero.settings')
+
+application = get_asgi_application()
