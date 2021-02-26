6dc5c31f901856086f8ee8c3713957222ca9817d
BrunoTerre <brunoeterres@gmail.com>
2021-02-25 22:42:45 -0300

    New Signin Form

--- app/authentication/forms.py
+++ app/authentication/forms.py
@@ -4,9 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=30)
-    email = forms.EmailField(max_length=200)
+    first_name = forms.CharField(max_length=100, help_text='First Name')
+    last_name = forms.CharField(max_length=100, help_text='Last Name')
+    email = forms.EmailField(max_length=200, help_text='Email')
+
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name','email', 'password1', 'password2', )
